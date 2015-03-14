244667d7ba86066bed5c7aab679d13827a18b222
Joshua May <notjosh@gmail.com>
2015-03-14 18:58:49 +0100

    Hiding dummy content in notes UI

--- Flaredown/src/FDNotesViewController.m
+++ Flaredown/src/FDNotesViewController.m
@@ -53,9 +53,9 @@
                                                object:nil];
     self.textView.text = [[[FDModelManager sharedManager] entry] notes];
 
-
+#if 0
     self.textView.text = @"#Lorem ipsum dolor #sit er elit #la!met, consec#tetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
-
+#endif
 }
 
 - (void)didReceiveMemoryWarning {
