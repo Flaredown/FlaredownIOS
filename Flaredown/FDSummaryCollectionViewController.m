765e69eddd2a642e6fc6f2d6f8c6dc370f538013
Cole Cunningham <cole@Coles-MacBook-Pro.local>
2015-05-26 13:08:50 -0700

    Updated style for summary page

--- Flaredown/FDSummaryCollectionViewController.m
+++ Flaredown/FDSummaryCollectionViewController.m
@@ -11,13 +11,17 @@
 #import "FDStyle.h"
 #import "FDModelManager.h"
 
-#define CONDITION_TITLE_SECTION 0
-#define CONDITION_BASE_SECTION (CONDITION_TITLE_SECTION+1)
+#define CONDITION_TITLE 0
+#define CONDITION_BASE (CONDITION_TITLE+1)
 #define CONDITION_COUNT [[_entry questionsForCatalog:@"conditions"] count]
+#define NO_CONDITIONS (CONDITION_COUNT == 0)
+#define CONDITION_END (CONDITION_BASE + (NO_CONDITIONS ? 1 : CONDITION_COUNT))
 
-#define SYMPTOM_TITLE_SECTION (CONDITION_BASE_SECTION+CONDITION_COUNT)
-#define SYMPTOM_BASE_SECTION (SYMPTOM_TITLE_SECTION+1)
+#define SYMPTOM_TITLE (CONDITION_END)
+#define SYMPTOM_BASE (SYMPTOM_TITLE+1)
 #define SYMPTOM_COUNT [[_entry questionsForCatalog:@"symptoms"] count]
+#define NO_SYMPTOMS (SYMPTOM_COUNT == 0)
+#define SYMPTOM_END (SYMPTOM_BASE + (NO_SYMPTOMS ? 1 : SYMPTOM_COUNT))
 
 @interface FDSummaryCollectionViewController ()
 
@@ -29,11 +33,21 @@ static NSString * const TitleIdentifier = @"title";
 static NSString * const ItemNameIdentifier = @"itemName";
 static NSString * const ItemValueIdentifier = @"itemValue";
 static NSString * const ItemNoneIdentifier = @"itemNone";
+static NSString * const ItemNoValueIdentifier = @"itemNoValue";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.collectionView.contentInset = UIEdgeInsetsMake(10, 10, 10, 10);
+    //Style
+    self.collectionView.clipsToBounds = NO;
+    self.collectionView.frame = CGRectMake(self.view.frame.origin.x+10, self.view.frame.origin.y+10, self.view.frame.size.width-20, self.view.frame.size.height-20);
+    
+    self.view.layer.masksToBounds = YES;
+    [FDStyle addRoundedCornersToView:self.view];
+    [FDStyle addShadowToView:self.collectionView];
+    self.view.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2);
+    
+//    self.collectionView.contentInset = UIEdgeInsetsMake(10, 10, 10, 10);
     
     // Uncomment the following line to preserve selection between presentations
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -61,34 +75,35 @@ static NSString * const ItemNoneIdentifier = @"itemNone";
 }
 */
 
-- (IBAction)titleButton:(id)sender
+- (IBAction)questionButton:(id)sender
 {
     UICollectionViewCell *cell = (UICollectionViewCell *)[self parentCellForView:sender];
+    
     NSIndexPath *indexPath = [self.collectionView indexPathForCell:cell];
     NSUInteger section = [indexPath section];
     
-    if(section == CONDITION_TITLE_SECTION) {
+    if(section >= CONDITION_BASE && section < CONDITION_END) {
         //conditions
-        [_mainViewDelegate openPage:0];
-    } else if(section == SYMPTOM_TITLE_SECTION) {
+        [_mainViewDelegate openPage:0+section-CONDITION_BASE];
+    } else if(section >= SYMPTOM_BASE && section < SYMPTOM_END) {
         //symptoms
-        [_mainViewDelegate openPage:[[FDModelManager sharedManager] numberOfQuestionSections]-SYMPTOM_COUNT+1];
+        [_mainViewDelegate openPage:[[FDModelManager sharedManager] numberOfQuestionSections]-SYMPTOM_COUNT+1+section-SYMPTOM_BASE];
     }
 }
 
-- (IBAction)questionButton:(id)sender
+- (IBAction)noItemsButton:(id)sender
 {
-    UITableViewCell *cell = (UITableViewCell *)[self parentCellForView:sender];
+    UICollectionViewCell *cell = (UICollectionViewCell *)[self parentCellForView:sender];
     
     NSIndexPath *indexPath = [self.collectionView indexPathForCell:cell];
     NSUInteger section = [indexPath section];
-    
-    if(section >= CONDITION_BASE_SECTION && section < CONDITION_BASE_SECTION+CONDITION_COUNT) {
+
+    if(section >= CONDITION_BASE && section < CONDITION_END) {
         //conditions
-        [_mainViewDelegate openPage:0+section-CONDITION_BASE_SECTION];
-    } else if(section >= SYMPTOM_BASE_SECTION && section < SYMPTOM_BASE_SECTION+SYMPTOM_COUNT) {
+        [_mainViewDelegate openPage:0];
+    } else if(section >= SYMPTOM_BASE && section < SYMPTOM_END) {
         //symptoms
-        [_mainViewDelegate openPage:[[FDModelManager sharedManager] numberOfQuestionSections]-SYMPTOM_COUNT+1+section-SYMPTOM_BASE_SECTION];
+        [_mainViewDelegate openPage:[[FDModelManager sharedManager] numberOfQuestionSections]+1];
     }
 }
 
@@ -110,24 +125,27 @@ static NSString * const ItemNoneIdentifier = @"itemNone";
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
     
-    NSUInteger conditions = CONDITION_COUNT+1;
-    NSUInteger symptoms = SYMPTOM_COUNT+1;
+    NSUInteger conditions = CONDITION_END - CONDITION_BASE + 1;
+    NSUInteger symptoms = SYMPTOM_END - SYMPTOM_BASE + 1;
     
-    return conditions + symptoms;
+    return conditions+symptoms;
 }
 
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    if(section == CONDITION_TITLE_SECTION) {
+    if(section == CONDITION_TITLE) {
         return 1;
-    } else if(section < CONDITION_BASE_SECTION + CONDITION_COUNT) {
-        FDQuestion *question = [_entry questionsForCatalog:@"conditions"][section-CONDITION_BASE_SECTION];
+    } else if(section < CONDITION_END) {
+        if(NO_CONDITIONS)
+            return 1;
+        FDQuestion *question = [_entry questionsForCatalog:@"conditions"][section-CONDITION_BASE];
         return [self numberOfItemsForQuestion:question];
-    } else if(section == SYMPTOM_TITLE_SECTION) {
+    } else if(section == SYMPTOM_TITLE) {
         return 1;
-    } else if(section < SYMPTOM_BASE_SECTION + SYMPTOM_COUNT) {
-        int i = SYMPTOM_BASE_SECTION;
-        FDQuestion *question = [_entry questionsForCatalog:@"symptoms"][section-SYMPTOM_BASE_SECTION];
+    } else if(section < SYMPTOM_END) {
+        if(NO_SYMPTOMS)
+            return 1;
+        FDQuestion *question = [_entry questionsForCatalog:@"symptoms"][section-SYMPTOM_BASE];
         return [self numberOfItemsForQuestion:question];
     }
     return 0;
@@ -146,61 +164,70 @@ static NSString * const ItemNoneIdentifier = @"itemNone";
     NSUInteger section = [indexPath section];
     NSUInteger row = [indexPath row];
     
-    NSUInteger numConditions = [[_entry questionsForCatalog:@"conditions"] count];
-    NSUInteger numSymptoms = [[_entry questionsForCatalog:@"symptoms"] count];
-    
     UICollectionViewCell *cell;
     
-    if(section == CONDITION_TITLE_SECTION) {
+    if(section == CONDITION_TITLE) {
         cell = [collectionView dequeueReusableCellWithReuseIdentifier:TitleIdentifier forIndexPath:indexPath];
         
         //1 Button
-        UIButton *button = (UIButton *)[cell viewWithTag:1];
-        [button setTitle:FDLocalizedString(@"conditions") forState:UIControlStateNormal];
+        UILabel *label = (UILabel *)[cell viewWithTag:1];
+        [label setText:FDLocalizedString(@"conditions")];
         
-    } else if(section < CONDITION_BASE_SECTION + CONDITION_COUNT) {
-        
-        FDQuestion *question = [_entry questionsForCatalog:@"conditions"][section-CONDITION_BASE_SECTION];
-        FDResponse *response = [_entry responseForQuestion:question];
-        
-        if(row == 0) {
-            cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemNameIdentifier forIndexPath:indexPath];
-            
-            //1 Label
-            UILabel *label = (UILabel *)[cell viewWithTag:1];
-            [label setText:[question name]];
-            
-        } else if(response && [response value] > 0) {
-            cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemValueIdentifier forIndexPath:indexPath];
-            [FDStyle addSmallRoundedCornersToView:cell];
-        } else {
+    } else if(section < CONDITION_END) {
+        if(NO_CONDITIONS) {
             cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemNoneIdentifier forIndexPath:indexPath];
+        } else {
+            FDQuestion *question = [_entry questionsForCatalog:@"conditions"][section-CONDITION_BASE];
+            FDResponse *response = [_entry responseForQuestion:question];
+            
+            if(row == 0) {
+                cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemNameIdentifier forIndexPath:indexPath];
+                
+                //1 Label
+                UILabel *label = (UILabel *)[cell viewWithTag:1];
+                [label setText:[question name]];
+                
+            } else if(response && [response value] > 0) {
+                cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemValueIdentifier forIndexPath:indexPath];
+                
+                //1 Button
+                UIButton *button = (UIButton *)[cell viewWithTag:1];
+                [FDStyle addSmallRoundedCornersToView:button];
+            } else {
+                cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemNoValueIdentifier forIndexPath:indexPath];
+            }
         }
         
-    } else if(section == SYMPTOM_TITLE_SECTION) {
+    } else if(section == SYMPTOM_TITLE) {
         cell = [collectionView dequeueReusableCellWithReuseIdentifier:TitleIdentifier forIndexPath:indexPath];
         
         //1 Button
-        UIButton *button = (UIButton *)[cell viewWithTag:1];
-        [button setTitle:FDLocalizedString(@"symptoms") forState:UIControlStateNormal];
+        UILabel *label = (UILabel *)[cell viewWithTag:1];
+        [label setText:FDLocalizedString(@"symptoms")];
         
-    } else if(section < SYMPTOM_BASE_SECTION + SYMPTOM_COUNT) {
-        
-        FDQuestion *question = [_entry questionsForCatalog:@"symptoms"][section-SYMPTOM_BASE_SECTION];
-        FDResponse *response = [_entry responseForQuestion:question];
-        
-        if(row == 0) {
-            cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemNameIdentifier forIndexPath:indexPath];
-            
-            //1 Label
-            UILabel *label = (UILabel *)[cell viewWithTag:1];
-            [label setText:[question name]];
-            
-        } else if(response && [response value] > 0) {
-            cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemValueIdentifier forIndexPath:indexPath];
-            [FDStyle addSmallRoundedCornersToView:cell];
-        } else {
+    } else if(section < SYMPTOM_END) {
+        if(NO_SYMPTOMS) {
             cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemNoneIdentifier forIndexPath:indexPath];
+        } else {
+            FDQuestion *question = [_entry questionsForCatalog:@"symptoms"][section-SYMPTOM_BASE];
+            FDResponse *response = [_entry responseForQuestion:question];
+            
+            if(row == 0) {
+                cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemNameIdentifier forIndexPath:indexPath];
+                
+                //1 Label
+                UILabel *label = (UILabel *)[cell viewWithTag:1];
+                [label setText:[question name]];
+                
+            } else if(response && [response value] > 0) {
+                cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemValueIdentifier forIndexPath:indexPath];
+                
+                //1 Button
+                UIButton *button = (UIButton *)[cell viewWithTag:1];
+                [FDStyle addSmallRoundedCornersToView:button];
+            } else {
+                cell = [collectionView dequeueReusableCellWithReuseIdentifier:ItemNoValueIdentifier forIndexPath:indexPath];
+            }
         }
     }
     return cell;
@@ -213,33 +240,46 @@ static NSString * const ItemNoneIdentifier = @"itemNone";
     NSUInteger section = [indexPath section];
     NSUInteger row = [indexPath row];
     
-    if(section == CONDITION_TITLE_SECTION) {
-        return CGSizeMake(collectionView.frame.size.width, 55);
-    } else if(section < CONDITION_BASE_SECTION + CONDITION_COUNT) {
-        FDQuestion *question = [_entry questionsForCatalog:@"conditions"][section-CONDITION_BASE_SECTION];
-        FDResponse *response = [_entry responseForQuestion:question];
-        
-        if(row == 0)
-            return CGSizeMake(collectionView.frame.size.width, 35);
-        else if(response && [response value] > 0)
-            return CGSizeMake(54, 15);
-        else
-            return CGSizeMake(75, 25);
-    } else if(section == SYMPTOM_TITLE_SECTION) {
-        return CGSizeMake(collectionView.frame.size.width, 55);
-    } else if(section < SYMPTOM_BASE_SECTION + SYMPTOM_COUNT) {
-        FDQuestion *question = [_entry questionsForCatalog:@"symptoms"][section-SYMPTOM_BASE_SECTION];
-        FDResponse *response = [_entry responseForQuestion:question];
-        
-        if(row == 0)
-            return CGSizeMake(collectionView.frame.size.width, 35);
-        else if(response && [response value] > 0)
-            return CGSizeMake(54, 15);
-        else
-            return CGSizeMake(75, 25);
+    if(section == CONDITION_TITLE) {
+        return CGSizeMake(collectionView.frame.size.width, 40);
+    } else if(section < CONDITION_END) {
+        if(NO_CONDITIONS) {
+            return CGSizeMake(collectionView.frame.size.width, 50);
+        } else {
+            FDQuestion *question = [_entry questionsForCatalog:@"conditions"][section-CONDITION_BASE];
+            FDResponse *response = [_entry responseForQuestion:question];
+            
+            if(row == 0)
+                return CGSizeMake(collectionView.frame.size.width, 30);
+            else if(response && [response value] > 0)
+                return CGSizeMake(54, 15);
+            else
+                return CGSizeMake(115, 35);
+        }
+    } else if(section == SYMPTOM_TITLE) {
+        return CGSizeMake(collectionView.frame.size.width, 40);
+    } else if(section < SYMPTOM_END) {
+        if(NO_SYMPTOMS) {
+            return CGSizeMake(collectionView.frame.size.width, 50);
+        } else {
+            FDQuestion *question = [_entry questionsForCatalog:@"symptoms"][section-SYMPTOM_BASE];
+            FDResponse *response = [_entry responseForQuestion:question];
+            
+            if(row == 0)
+                return CGSizeMake(collectionView.frame.size.width, 30);
+            else if(response && [response value] > 0)
+                return CGSizeMake(54, 15);
+            else
+                return CGSizeMake(115, 35);
+            }
     }
     return CGSizeZero;
 }
+
+//- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+//{
+//    return 0;
+//}
 
 #pragma mark <UICollectionViewDelegate>
 
